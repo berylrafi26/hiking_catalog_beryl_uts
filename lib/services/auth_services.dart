e608cdd8faa68b450538f1f161049531a86841b4
Beryl <berylrafi3@gmail.com>
2026-04-21 01:54:04 +0700

    kelar bikin auth services

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-
   Future<User?> register(String email, String password) async {
     final result = await _auth.createUserWithEmailAndPassword(
       email: email,
@@ -14,3 +13,24 @@ class AuthService {
 
     return result.user;
   }
+
+  Future<User?> login(String email, String password) async {
+    final result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    await result.user!.reload();
+    final user = _auth.currentUser;
+
+    if (user != null && user.emailVerified) {
+      return user;
+    } else {
+      throw Exception("Email belum diverifikasi");
+    }
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
