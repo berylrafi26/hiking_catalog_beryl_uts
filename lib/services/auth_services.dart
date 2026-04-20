ae21c422ddcf00ca19bfd830144d56beb9c9f3ff
Beryl <berylrafi3@gmail.com>
2026-04-21 01:53:45 +0700

    bikin file auth service untuk autentikasi

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,1 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  Future<User?> register(String email, String password) async {
+    final result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    await result.user!.sendEmailVerification();
+
+    return result.user;
+  }
