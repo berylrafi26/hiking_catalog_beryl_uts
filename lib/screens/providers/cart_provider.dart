f7e3af2592a996cb78207dc97857af18e214a6a9
Beryl <berylrafi3@gmail.com>
2026-04-23 04:44:34 +0700

    selesai bikin cart provider

--- lib/screens/providers/cart_provider.dart
+++ lib/screens/providers/cart_provider.dart
@@ -30,4 +30,25 @@ class CartProvider with ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void decreaseItem(String id) {
+    if (!_items.containsKey(id)) return;
+
+    if (_items[id]!.quantity > 1) {
+      _items[id]!.quantity--;
+    } else {
+      _items.remove(id);
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String id) {
+    _items.remove(id);
+    notifyListeners();
+  }
+
+  void clearCart() {
+    _items.clear();
+    notifyListeners();
+  }
 }
