73a32408597c091a5ce0a77a55d11f57b5ebd629
Beryl <berylrafi3@gmail.com>
2026-04-23 09:15:57 +0700

    rombak cart_provider

--- lib/screens/providers/cart_provider.dart
+++ lib/screens/providers/cart_provider.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/cart_item.dart';
 
 class CartProvider with ChangeNotifier {
-  final Map<String, CartItem> _items = {};
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items => _items;
 
@@ -17,21 +19,23 @@ class CartProvider with ChangeNotifier {
     );
   }
 
-  void addItem({
+  Future<void> addItem({
     required String id,
     required String name,
     required int price,
     required String image,
-  }) {
+  }) async {
     if (_items.containsKey(id)) {
       _items[id]!.quantity++;
     } else {
       _items[id] = CartItem(id: id, name: name, price: price, image: image);
     }
+
+    await saveCart();
     notifyListeners();
   }
 
-  void decreaseItem(String id) {
+  Future<void> decreaseItem(String id) async {
     if (!_items.containsKey(id)) return;
 
     if (_items[id]!.quantity > 1) {
@@ -39,16 +43,60 @@ class CartProvider with ChangeNotifier {
     } else {
       _items.remove(id);
     }
+
+    await saveCart();
     notifyListeners();
   }
 
-  void removeItem(String id) {
+  Future<void> removeItem(String id) async {
     _items.remove(id);
+    await saveCart();
     notifyListeners();
   }
 
-  void clearCart() {
+  Future<void> clearCart() async {
     _items.clear();
+    await saveCart();
     notifyListeners();
+  }
+
+  Future<void> saveCart() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final cartData = _items.map(
+      (key, item) => MapEntry(key, {
+        'id': item.id,
+        'name': item.name,
+        'price': item.price,
+        'image': item.image,
+        'quantity': item.quantity,
+      }),
+    );
+
+    await prefs.setString('cart', jsonEncode(cartData));
+  }
+
+  Future<void> loadCart() async {
+    final prefs = await SharedPreferences.getInstance();
+    final data = prefs.getString('cart');
+
+    if (data != null) {
+      final decoded = jsonDecode(data) as Map<String, dynamic>;
+
+      _items = decoded.map(
+        (key, value) => MapEntry(
+          key,
+          CartItem(
+            id: value['id'],
+            name: value['name'],
+            price: value['price'],
+            image: value['image'],
+            quantity: value['quantity'],
+          ),
+        ),
+      );
+
+      notifyListeners();
+    }
   }
 }
